47f5462dfa723d4080c7df5d0d74b27b35e251bf
MaryannChidimma <okonkwo20chidimma@gmail.com>
2022-10-15 22:04:18 +0100

    holds all reminder data

---
+++ src/database/reminder.ts
@@ -0,0 +1,1 @@
+[{"user":1,"description":"Drink Coffee","id":1,"date":"2022-10-15T20:48:16.020Z"},{"id":2,"date":"2022-10-15T20:48:27.504Z","user":2,"description":"Drink Coffee"},{"id":3,"date":"2022-10-15T20:48:30.747Z","user":2,"description":"Drink Coffee"},{"id":4,"date":"2022-10-15T20:48:31.895Z","user":2,"description":"Drink Coffee"}]
