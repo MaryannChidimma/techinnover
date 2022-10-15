83395e38248d808961a2dfd1ef86859a3ce15ad9
MaryannChidimma <okonkwo20chidimma@gmail.com>
2022-10-15 22:05:40 +0100

    added validators

---
+++ src/validators/index.ts
@@ -0,0 +1,18 @@
+import Joi from "joi";
+import express from "express";
+import { BadRequestError } from "../../lib/appError";
+
+type Source = "body" | "query" | "params" | "headers";
+
+const Validator =
+  (schema: Joi.ObjectSchema, source: Source) =>
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const result = schema.validate(req[source]);
+
+    if (result.error) {
+      throw new BadRequestError(result.error.message);
+    }
+    next();
+  };
+
+export { Validator };
